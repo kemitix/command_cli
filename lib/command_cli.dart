dcc0717dad5979343aac4a4c4b85c69a9028c2ff
Paul Campbell <pcampbell@kemitix.net>
2022-04-15 21:20:41 +0100

    Export missing classes

--- lib/command_cli.dart
+++ lib/command_cli.dart
@@ -1,3 +1,6 @@
 export 'package:command_cli/src/command.dart';
 export 'package:command_cli/src/command_cli.dart';
 export 'package:command_cli/src/command_parent.dart';
+export 'package:command_cli/src/error_command.dart';
+export 'package:command_cli/src/help_command.dart';
+export 'package:command_cli/src/inline_command.dart';
---
+++ lib/command_logging.dart
@@ -0,0 +1,1 @@
+export 'package:command_cli/src/log_service.dart';

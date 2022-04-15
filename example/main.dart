406747394f92d104988ac56b787916ab552a7dbb
Paul Campbell <pcampbell@kemitix.net>
2022-04-15 20:51:03 +0100

    example: expand example

--- example/main.dart
+++ example/main.dart
@@ -4,17 +4,39 @@ import 'package:command_cli/src/log_service.dart';
 
 void main(List<String> arguments) {
   CommandCli(arguments).findCommand([
+    /// Inline allows simple declarations without creating a class
     InlineCommand(
-        'help', () async => LogService.info('This is where I would help you'),
-        hint: 'hints and tips'),
+      /// the command name
+      'help',
+
+      /// simple actions can be declared inline, with or without braces
+      () async => LogService.info('This is where I would help you'),
+
+      /// message to display when showing help or the list of available commands
+      hint: 'hints and tips',
+
+      /// TODO
+      // codeSample: ''
+
+      /// TODO
+      ///maxParameters: 1
+    ),
+
+    /// The second parameter can be a reference to a function
     InlineCommand('foo', fooCommand, hint: 'foo cmd'),
-    InlineCommand('bar', () async => LogService.info('This is BAR!'),
-        hint: 'bar cmd'),
+
+    /// Call a function to get a list of single command or a module
     bazModuleCommands(),
+
+    /// Use a class extending CommandParent to create a module
+    GavModule(),
   ]).execute();
 }
 
+Future<void> fooCommand() async => LogService.info('This is FOO!');
+
 CommandParent bazModuleCommands() {
+  /// CommandParent defines a branch or module
   return CommandParent(
       'baz',
       [
@@ -30,4 +52,10 @@ CommandParent bazModuleCommands() {
       hint: 'more commands');
 }
 
-Future<void> fooCommand() async => LogService.info('This is FOO!');
+class GavModule extends CommandParent {
+  GavModule()
+      : super('gav', [
+          InlineCommand(
+              'hello', () async => LogService.info('Hello from the Gav Module'))
+        ]);
+}
